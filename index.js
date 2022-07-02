5e02ca6c8464609dd753fc65b107308aa9b7f246
Danielle Cristina <dnaielle.trybe@gmail.com>
2022-07-01 21:38:16 -0300

    Requisito 5

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const StrangerThingsService = require('./services/StrangerThings');
 
 const app = express();
 
-// const port = process.env.PORT;
+const port = process.env.PORT;
 
 const strangerThingsRepository = new StrangerThingsRepository(
   strangerThingsDataset,
@@ -19,7 +19,7 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === 'true';
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
@@ -30,6 +30,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
+app.listen(port, () => {
+  console.log(`Escutando na porta ${port}`);
 });
